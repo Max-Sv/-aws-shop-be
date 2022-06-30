264d5903dc6afa7443bd75fd9bd51929dae0400f
Maksim Svilionok <maksim.svilionok@LeverX.local>
2022-06-30 11:10:32 +0200

    feat: add transaction to post req

--- src/services/db-client.ts
+++ src/services/db-client.ts
@@ -61,20 +61,30 @@ export const addProduct  = async (params: any): Promise<any> => {
     const client = new Client(dbOptions);
 
     try {
-        const { title, description, price } =  params;
-
+        const { title, description, price, count } =  params;
         createProductValidator(title, description, price)
 
         await client.connect();
+        await client.query('BEGIN');
         const createdProduct = await client.query(`
             insert into products (title, description, price) 
             values ('${title}', '${description}', ${price})
             returning *
-            `)
+            `);
+        const { id } = createdProduct.rows[0];
+        const productCount = count || 0;
+        const stock = await client.query(`
+            insert into stocks (product_id, count)
+            values ('${id}', '${productCount}')
+            returning *
+            `);
+        await client.query(`COMMIT`);
 
-        return {...createdProduct};
+        return {...createdProduct, count: stock.rows[0].count };
     } catch (error) {
+        await client.query(`ROLLBACK`);
         throw new ServerError(error.message);
+
     } finally {
         await client.end();
     }
